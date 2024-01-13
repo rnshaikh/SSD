7ff51320ec11a47f5398c0b2b1720ef59baf7d2c
rnshaikh <rizwanshaikh2429@gmail.com>
2024-01-13 23:39:45 +0530

    updated match logic

--- Ride_Sharing/src/match.py
+++ Ride_Sharing/src/match.py
@@ -1,14 +1,17 @@
 import math
 import conf
+
 from heapq import heappush, heappop
+from src.utils import my_round
 
 
 
 def calculate_ecd(start_x_co, start_y_co, end_x_co, end_y_co):
-
+	
 	ans = (end_x_co - start_x_co) ** conf.TWO + (end_y_co - start_y_co) ** conf.TWO
 	ans = math.sqrt(ans)
-	ans = math.ceil(ans*conf.PERC_DIVISOR)/conf.PERC_DIVISOR
+	ans = my_round(ans, conf.TWO)
+	ans = (ans*conf.PERC_DIVISOR)/conf.PERC_DIVISOR
 	return ans
 
 
@@ -25,8 +28,9 @@ class EcMatch(Match):
 		match_drivers = []
 
 		for driver in drivers:
-			ans = calculate_ecd(rider.x_co,rider.y_co, drivers[driver].x_co, drivers[driver].y_co)
-			if ans <= conf.MAX_DRIVER_SHOW:
-				heappush(match_drivers, (ans, driver))
+			if drivers[driver].is_available():
+				ans = calculate_ecd(rider.x_co,rider.y_co, drivers[driver].x_co, drivers[driver].y_co)
+				if ans <= conf.MAX_DRIVER_SHOW:
+					heappush(match_drivers, (ans, driver))
 		return match_drivers
 
