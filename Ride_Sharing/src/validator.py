96f2628bbb940a8a80ef49f971c998b702469a0e
rnshaikh <rizwanshaikh2429@gmail.com>
2024-01-15 00:45:03 +0530

    Updated validator

--- Ride_Sharing/src/validator.py
+++ Ride_Sharing/src/validator.py
@@ -1,4 +1,5 @@
 import conf
+from src.ride import Status
 
 
 def validate_rider(rider):
@@ -48,3 +49,23 @@ def validate_rider_already_exist(rider, riders):
 	if rider.id in riders:
 		raise Exception("RIDER_ALREADY_EXIST")
 		
+
+def check_ride_id_is_not_exist(ride_id, rides):
+
+	if ride_id not in rides:
+		raise Exception("INVALID_RIDE")
+
+def check_ride_is_completed(ride):
+
+	if not ride.is_ride_completed():
+		raise Exception("RIDE_NOT_COMPLETED")
+
+def check_ride_exists(ride):
+
+	if not ride:
+		raise Exception("INVALID_RIDE")
+
+def check_ride_completed(ride):
+
+	if ride.status == Status.COMPLETED.value:
+		raise Exception("INVALID_RIDE")
