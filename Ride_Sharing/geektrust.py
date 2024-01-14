17c51b5e1a019ba8586078eaaea58c3b749c6948
rnshaikh <rizwanshaikh2429@gmail.com>
2024-01-15 00:43:51 +0530

    Updated conf

--- Ride_Sharing/geektrust.py
+++ Ride_Sharing/geektrust.py
@@ -1,12 +1,12 @@
 
 import sys
-
+import conf
 from src.program_driver import program_driver
 
 
 def main():
 
-	file_path = sys.argv[1]
+	file_path = sys.argv[conf.ONE_INIT]
 	program_driver(file_path)
 	
 if __name__ == "__main__":
