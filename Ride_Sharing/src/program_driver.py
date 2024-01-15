98f0368a54576edafa826709252986c2abd74db5
rnshaikh <rizwanshaikh2429@gmail.com>
2024-01-16 00:44:02 +0530

    updated program driver

--- Ride_Sharing/src/program_driver.py
+++ Ride_Sharing/src/program_driver.py
@@ -4,6 +4,26 @@ from src.ride_sharing  import RideSharing
 from src.utils import format_params
 
 
+def execute_action_with_obj(action_info, sp_str, ride_sharing):
+
+	if action_info and action_info.get('obj', None):
+		param_count = action_info.get('param_count')
+		params = format_params(sp_str, param_count)
+		obj = action_info.get('obj')(*params)
+		action = action_info['action']
+		getattr(ride_sharing, action)(obj)
+
+
+def execute_action_without_obj(action_info, sp_str, ride_sharing):
+
+	if action_info and not action_info.get('obj', None):
+		param_count = action_info.get('param_count')
+		params = format_params(sp_str, param_count)
+		action = action_info['action']
+		getattr(ride_sharing, action)(*params)
+
+
+
 def program_driver(file_path):
 
 	fs = open(file_path, 'r')
@@ -14,16 +34,19 @@ def program_driver(file_path):
 		sp_str = query.split(" ")
 		action_info = conf.COMMANDS.get(sp_str[conf.ZERO_INIT], None)
 
-		if action_info and action_info.get('obj', None):
-			param_count = action_info.get('param_count')
-			params = format_params(sp_str, param_count)
-			obj = action_info.get('obj')(*params)
-			action = action_info['action']
-			getattr(ride_sharing, action)(obj)
+		execute_action_with_obj(action_info, sp_str, ride_sharing)
+		execute_action_without_obj(action_info, sp_str, ride_sharing)
 
-		if action_info and not action_info.get('obj', None):
-			param_count = action_info.get('param_count')
-			params = format_params(sp_str, param_count)
-			action = action_info['action']
-			getattr(ride_sharing, action)(*params)
+		# if action_info and action_info.get('obj', None):
+		# 	param_count = action_info.get('param_count')
+		# 	params = format_params(sp_str, param_count)
+		# 	obj = action_info.get('obj')(*params)
+		# 	action = action_info['action']
+		# 	getattr(ride_sharing, action)(obj)
+
+		# if action_info and not action_info.get('obj', None):
+		# 	param_count = action_info.get('param_count')
+		# 	params = format_params(sp_str, param_count)
+		# 	action = action_info['action']
+		# 	getattr(ride_sharing, action)(*params)
 
