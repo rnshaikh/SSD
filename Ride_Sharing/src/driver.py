ec80ace614a18c718510036ac10c480a7259fb0b
rnshaikh <rizwanshaikh2429@gmail.com>
2024-02-08 00:52:14 +0530

    added getter for id and location

--- Ride_Sharing/src/driver.py
+++ Ride_Sharing/src/driver.py
@@ -8,14 +8,18 @@ class Driver:
 		self.y_co = y_co
 		self.available = conf.TRUE
 
-	def is_available(self):
+	def get_id(self):
+		return self.id 
 
+	def is_available(self):
 		return self.available 
 
 	def update_available(self):
-
 		self.available = not self.available
 
 	def update_location(self, x_co, y_co):
 		self.x_co = x_co
 		self.y_co = y_co
+
+	def get_location(self):
+		return (self.x_co, self.y_co)
