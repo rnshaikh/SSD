2cb13849a35414599ab9337ecb822182fd4fc968
rnshaikh <rizwanshaikh2429@gmail.com>
2024-02-12 01:11:12 +0530

    Added conf

---
+++ MetroCard/conf.py
@@ -0,0 +1,30 @@
+INIT_ONE=1
+INIT_ZERO=0
+
+COMMANDS = {
+	"BALANCE": {
+		"func": "add_metrocard"
+
+	},
+	"CHECK_IN": {
+		"func": "check_in"
+
+	},
+	"PRINT_SUMMARY":{
+		"func": "print_summary"
+	}
+}
+
+PERSON_TYPE_COST = {
+	"ADULT": 200,
+	"SENIOR_CITIZEN": 100,
+	"KID": 50
+}
+
+SERVICE_COST = 2
+RETURN_TRAVEL_DISCOUNT = 50
+HUNDRED_DIVISOR = 100
+
+STATION_1 = "CENTRAL"
+STATION_2 = "AIRPORT"
+
